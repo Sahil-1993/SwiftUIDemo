58f3b6f020d13866c420a4f07897566a0c0ea80f
Sahil-1993 <55412573+Sahil-1993@users.noreply.github.com>
2022-05-18 12:14:17 -0400

    Add files via upload

---
+++ SwiftUICodingPractice/SwiftUICodingPractice/SwiftUICodingPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICodingPracticeApp.swift
+//  SwiftUICodingPractice
+//
+//  Created by Sahil Arora on 2022-05-17.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICodingPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
